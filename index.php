d6e0b0b2087030b0fd23101a5264af8b08a5232d
JarekCodeCabin <jarek@codecabin.co.za>
2018-10-25 14:04:45 +0200

    Test 13

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Document</title>
     <style>
         body {
-            background-color: #f2f2f0;
+            background-color: #f2f2f2;
         }
         .main {
             width: 960px;
